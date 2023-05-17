41db6cbed0c2ef7ec2ebcddad74f3a6a9e69da19
2210391 <2210391@ecc.ac.jp>
2023-05-17 13:29:02 +0900

    変更

--- src/components/drawermenu.tsx
+++ src/components/drawermenu.tsx
@@ -6,11 +6,21 @@ export default function DrawerMenu(){
         <>
             <div>
                 <ul>
-                    <li>口コミ</li>
-                    <li>投稿する</li>
-                    <li>アカウント</li>
-                    <li>イベント・ニュース</li>
-                    <li>つながる</li>
+                    <li>
+                        <a href="/coment.tsx">口コミ</a>
+                    </li>
+                    <li>
+                        <a href="/post.tsx">投稿する</a>
+                    </li>
+                    <li>
+                        <a href="/account.tsx">アカウント</a>
+                    </li>
+                    <li>
+                        <a href="/event.tsx">イベント・ニュース</a>
+                    </li>
+                    <li>
+                        <a href="/connect.tsx">つながる</a>
+                    </li>
                 </ul>
             </div>
         </>

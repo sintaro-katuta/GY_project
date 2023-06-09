8a00c062dbe2a4ba1e78a50d7093193c90878b10
LAPTOP-9V7SEE3A\2210436 <2210436@ecc.ac.jp>
2023-06-09 16:12:16 +0900

    ヘッダーデザインできました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import Image from 'next/image';
 import Link from "next/link";
 import { getAuth, signOut } from "firebase/auth";
+import Style from '../styles/Header.module.css';
 
 export default function Header() {
     const router = useRouter();
@@ -23,22 +24,12 @@ export default function Header() {
         signOut(auth)
     }
     return (
-        <>
-            <header>
-                <p>ヘッダー</p>
-                {/* <Image
-                    src={"/image/hamburger.svg"}
-                    height={20} width={20} alt='ハンバーガー'
-                    onClick={() => setDrawerOpened(true)}
-                />
-                <Drawer
-                    anchor={'left'}
-                    open={drawerOpened}
-                    onClose={() => setDrawerOpened(false)}
-                    PaperProps={{ style: { width: '70%' } }}>
-                    <DrawerMenu />
-                </Drawer> */}
-
+        <header className={Style.container}>
+            <div className={Style.title}>
+                <p >~思い出巡る・心繋がる~</p>
+                <p>IRでの体験や思い出を共有するSNSサービス</p>
+            </div>
+            <div className={Style.list}>
                 <Link href={"http://localhost:8888"}>リゾートサイトへ</Link><br />
                 <Link href="/">Top</Link>
                 {paths.map((x, i) => {
@@ -48,36 +39,65 @@ export default function Header() {
                         </Link>
                     )
                 })}
-                <ul>
+                <ul className={Style.ul}>
                     <li>
-                        <Link href={"/memories"}>
+                        <Link href={"/memories"} className={Style.li}>
                             みんなの思い出
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/album"}>
+                        <Link href={"/album"} className={Style.li}>
                             みんなのアルバム
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/connect"}>
+                        <Link href={"/connect"} className={Style.li}>
                             繋がる
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/event"}>
+                        <Link href={"/event"} className={Style.li}>
                             特集・イベント
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/account"}>
+                        <Link href={"/account"} className={Style.li}>
                             アカウント
                         </Link>
                     </li>
                 </ul>
-                <button onClick={() => toPost()}>投稿する</button>
+                <button className={Style.contribution} onClick={() => toPost()}>投稿する</button>
                 <button onClick={() => signout()}>サインアウト</button>
-            </header>
-        </>
+            </div>
+            {/* <Image
+                src={"/image/hamburger.svg"}
+                height={20} width={20} alt='ハンバーガー'
+                onClick={() => setDrawerOpened(true)}
+            />
+            <Drawer
+                anchor={'left'}
+                open={drawerOpened}
+                onClose={() => setDrawerOpened(false)}
+                PaperProps={{ style: { width: '70%' } }}>
+                <DrawerMenu />
+            </Drawer> */}
+
+            <Image className={Style.top}
+            src = {"/image/top.png"}
+            width = {360} height = {126} alt='トップ'
+            />
+
+            <Image className={Style.icon}
+            src = {"/image/Group 53.png" }
+            width = {360} height = {126} alt='アイコン'
+            />
+            <Image className={Style.header}
+            src = {"/image/header.png" }
+            width = {360} height = {126} alt='ヘッダー'
+            />
+            <div className={Style.div}>
+                
+            </div>
+        </header>
     )
 }

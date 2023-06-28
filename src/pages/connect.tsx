565a63f13e7ecbd984ad016c06e0ea17c07b7186
2210040 <2210040@ecc.ac.jp>
2023-06-28 10:03:25 +0900

    一旦コミット

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,9 +1,26 @@
+import { db } from '../lib/firebase.config';
 import SubHeader from "../components/subheader";
+import { useEffect } from "react"
+import { collection, getDocs } from "firebase/firestore";
+
 export default function Connect() {
+    useEffect(() => {
+        (async () => {
+            const posts = collection(db, "posts")
+            const postsSnapShot = await getDocs(posts)
+            const newPostList = postsSnapShot.docs.map((doc: any) => {
+                const item = doc.data()
+                item.id = doc.id
+                return item
+            })
+            console.log(newPostList)
+        })()
+    }, [])
     return (
         <div>
-            <SubHeader/>
+            <SubHeader />
             <h1>繋がる</h1>
+
         </div>
     )
 }
